69d494452650ebc2fc41c4a5b7190e59e0015b82
yeon75 <yeon007500@gmail.com>
2024-08-01 16:55:12 +0900

    0731

--- topic_final/src/msub.cpp
+++ topic_final/src/msub.cpp
@@ -6,14 +6,15 @@
 using namespace std;
 using namespace std::chrono_literals;
 
-class Msub2 : public rclcpp::Node
+class Msub : public rclcpp::Node
 {
 public:
-    Msub2() : Node("msub2")
-    {
-        auto qos_profile = rclcpp::QoS(rclcpp::KeepLast(10));
-        _sub = this->create_subscription<std_msgs::msg::String>("message2", qos_profile, std::bind(&Msub2::sub_helloworld_msg, this, std::placeholders::_1));
-    }
+    Msub():
+        Node("msub")
+        {
+            auto qos_profile = rclcpp::QoS(rclcpp::KeepLast(10));
+            _sub = this->create_subscription<std_msgs::msg::String>("message1", qos_profile, std::bind(&Msub::sub_helloworld_msg, this, std::placeholders::_1));
+        }
 
 private:
     rclcpp::Subscription<std_msgs::msg::String>::SharedPtr _sub;
@@ -26,7 +27,7 @@ private:
 int main(int argc, char *argv[])
 {
     rclcpp::init(argc, argv);
-    auto node = std::make_shared<Msub2>();
+    auto node = std::make_shared<Msub>();
     rclcpp::spin(node);
     rclcpp::shutdown();
     return 0;
--- topic_final/src/msub2.cpp
+++ topic_final/src/msub2.cpp
@@ -6,15 +6,14 @@
 using namespace std;
 using namespace std::chrono_literals;
 
-class Msub : public rclcpp::Node
+class Msub2 : public rclcpp::Node
 {
 public:
-    Msub():
-        Node("msub")
-        {
-            auto qos_profile = rclcpp::QoS(rclcpp::KeepLast(10));
-            _sub = this->create_subscription<std_msgs::msg::String>("message1", qos_profile, std::bind(&Msub::sub_helloworld_msg, this, std::placeholders::_1));
-        }
+    Msub2() : Node("msub2")
+    {
+        auto qos_profile = rclcpp::QoS(rclcpp::KeepLast(10));
+        _sub = this->create_subscription<std_msgs::msg::String>("message2", qos_profile, std::bind(&Msub2::sub_helloworld_msg, this, std::placeholders::_1));
+    }
 
 private:
     rclcpp::Subscription<std_msgs::msg::String>::SharedPtr _sub;
@@ -27,7 +26,7 @@ private:
 int main(int argc, char *argv[])
 {
     rclcpp::init(argc, argv);
-    auto node = std::make_shared<Msub>();
+    auto node = std::make_shared<Msub2>();
     rclcpp::spin(node);
     rclcpp::shutdown();
     return 0;
