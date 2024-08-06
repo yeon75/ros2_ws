cf02d24b6b615ed26d9456abd11cda96d8ae98a7
yeon75 <yeon007500@gmail.com>
2024-08-06 17:12:34 +0900

    0806

--- arithmetic/include/arithmetic/operator.hpp
+++ arithmetic/include/arithmetic/operator.hpp
@@ -2,9 +2,11 @@
 #define OPERATOR_HPP
 
 #include "interface_example/srv/arithmetic_operator.hpp"
+#include "rcl_interfaces/msg/parameter_event.hpp"
 #include "rclcpp/rclcpp.hpp"
 #include "rcutils/cmdline_parser.h"
 #include <chrono>
+#include <random>
 
 using namespace std::chrono_literals;
 class Operator : public rclcpp::Node
@@ -14,9 +16,13 @@ public:
     Operator(const rclcpp::NodeOptions &options = rclcpp::NodeOptions());
 
 private:
+    bool _random_state;
+    int8_t _arithmetic_operator;
+    rclcpp::Subscription<rcl_interfaces::msg::ParameterEvent>::SharedPtr _parameter_event_sub;
     rclcpp::Client<ArithmeticOperator>::SharedPtr _service;
     rclcpp::TimerBase::SharedPtr _timer;
     void timer_callback();
     void response_callback(rclcpp::Client<ArithmeticOperator>::SharedFuture future);
+    void param_event_callback(const rcl_interfaces::msg::ParameterEvent::SharedPtr event);
 };
 #endif // OPERATOR_HPP
--- arithmetic/src/arithmetic/operator.cpp
+++ arithmetic/src/arithmetic/operator.cpp
@@ -3,8 +3,30 @@
 Operator::Operator(const rclcpp::NodeOptions &options)
     : Node("operator", options)
 {
+
+    auto qos_profile = rclcpp::QoS(rclcpp::KeepLast(10));
     _service = create_client<ArithmeticOperator>("arithmetic_operator");
     _timer = create_wall_timer(1s, std::bind(&Operator::timer_callback, this));
+
+    declare_parameter("random", true);
+    _random_state = get_parameter("random").get_value<bool>();
+    _parameter_event_sub = create_subscription<rcl_interfaces::msg::ParameterEvent>(
+        "/parameter_events",
+        qos_profile,
+        std::bind(&Operator::param_event_callback,
+                  this,
+                  std::placeholders::_1));
+}
+void Operator::param_event_callback(const rcl_interfaces::msg::ParameterEvent::SharedPtr event)
+{
+    for (auto &changed_parameter : event->changed_parameters)
+    {
+        if (changed_parameter.name == "random")
+        {
+            auto value = rclcpp::Parameter::from_parameter_msg(changed_parameter).get_value<bool>();
+            _random_state = value;
+        }
+    }
 }
 void Operator::timer_callback()
 {
@@ -18,9 +40,18 @@ void Operator::timer_callback()
     }
     // 보낼 변수 선언
     auto request = std::make_shared<ArithmeticOperator::Request>();
-    // TODO: random operator or parameter
-    request->arithmetic_operator = request->PLUS;
-
+    if (_random_state)
+    {
+        std::random_device rd;
+        std::mt19937 gen(rd());
+        std::uniform_int_distribution<int> dis(1, 4);
+        request->arithmetic_operator = (int8_t)dis(gen);
+        _arithmetic_operator = request->arithmetic_operator;
+    }
+    else
+    {
+        request->arithmetic_operator = _arithmetic_operator;
+    }
     auto result_future = _service->async_send_request(
         request, std::bind(&Operator::response_callback, this,
                            std::placeholders::_1));
@@ -31,7 +62,6 @@ void Operator::response_callback(rclcpp::Client<ArithmeticOperator>::SharedFutur
     auto status = future.wait_for(1s);
     if (status == std::future_status::ready)
     {
-        // TODO: operator 내부 변수 저장후 로깅.
         RCLCPP_INFO(get_logger(), "Result :%f", future.get()->arithmetic_result);
     }
     else
